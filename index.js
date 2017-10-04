80de15289f91c8afd8b3c5e12d6d46288b2e71aa
Denise Carty <denisecarty@comcast.net>
2017-10-04 21:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ function sayHiToGrandma (string) {
 
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } 
+  }
 
   if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 
-return "Are you eating enough?"
 }
